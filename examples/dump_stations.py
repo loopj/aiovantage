fb8921e4f4bfe28d8b4a5d7673bc3c86f6511e80
James Smith <james@loopj.com>
2023-03-24 00:27:43 -0700

    Dump stations example

---
+++ examples/dump_stations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from os.path import abspath, dirname
+from sys import path
+path.insert(1, dirname(dirname(abspath(__file__))))
+
+import asyncio
+from aiovantage import Vantage
+
+async def main():
+    async with Vantage("10.2.0.103", "administrator", "ZZuUw76CnL") as vantage:
+        for station in vantage.stations:
+            print(f"{station.name}:")
+
+            if station.buttons:
+                print("    Buttons:")
+                for button in station.buttons:
+                    print(f"        {button.name}")
+
+            if station.dry_contacts:
+                print("    Dry Contacts:")
+                for dry_contact in station.dry_contacts:
+                    print(f"        {dry_contact.name}")
+            
+            print()
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
