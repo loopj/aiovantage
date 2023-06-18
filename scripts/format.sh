3c597acbd3f9165f26afe4e70126770967dc280f
James Smith <james@loopj.com>
2023-06-18 14:31:12 -0700

    Remove scripts

--- scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-set -x
-
-ruff src examples --fix
-black src examples
--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy src examples
-ruff src examples
-black src examples --check
