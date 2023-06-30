edbafa346440030d59d9f5a76901c7607ebb23d9
James Smith <james@loopj.com>
2023-06-30 16:52:44 -0700

    Add more helpers to discovery module

--- src/aiovantage/discovery.py
+++ src/aiovantage/discovery.py
@@ -2,7 +2,11 @@
 from dataclasses import dataclass
 
 from aiovantage.command_client.commands import CommandClient
-from aiovantage.errors import ClientConnectionError, LoginRequiredError
+from aiovantage.errors import (
+    ClientConnectionError,
+    LoginFailedError,
+    LoginRequiredError,
+)
 
 
 @dataclass
@@ -12,6 +16,15 @@ class DiscoveredVantageController:
     host: str
     supports_ssl: bool
     requires_auth: bool
+
+
+async def _auth_required(client: CommandClient) -> bool:
+    # Check if authentication is required for the given client.
+    try:
+        await client.command("VERSION")
+        return False
+    except LoginRequiredError:
+        return True
 
 
 async def discover_controller(host: str) -> DiscoveredVantageController:
@@ -30,27 +43,53 @@ async def discover_controller(host: str) -> DiscoveredVantageController:
         # Try connecting with SSL first
         async with CommandClient(host) as client:
             supports_ssl = True
-            requires_auth = await is_auth_required(client)
+            requires_auth = await _auth_required(client)
     except ClientConnectionError:
         # If SSL fails, try connecting without SSL
         async with CommandClient(host, ssl=False) as client:
             supports_ssl = False
-            requires_auth = await is_auth_required(client)
+            requires_auth = await _auth_required(client)
 
     return DiscoveredVantageController(host, supports_ssl, requires_auth)
 
 
-async def is_auth_required(client: CommandClient) -> bool:
-    """Check if authentication is required for the given client.
+async def auth_required(host: str, ssl: bool = True) -> bool:
+    """Check if authentication is required for the given controller.
 
     Args:
-        client: The CommandClient to check.
+        host: The hostname/ip of the Vantage controller.
+        ssl: Whether to use SSL when connecting to the controller.
 
     Returns:
         True if authentication is required, False otherwise.
+
+    Raises:
+        ClientConnectionError: If a controller could not be reached.
+    """
+    async with CommandClient(host, ssl=ssl) as client:
+        return await _auth_required(client)
+
+
+async def valid_credentials(
+    host: str, username: str, password: str, ssl: bool = True
+) -> bool:
+    """Check if the given credentials are valid for the given controller.
+
+    Args:
+        host: The hostname/ip of the Vantage controller.
+        ssl: Whether to use SSL when connecting to the controller.
+        username: The username to check.
+        password: The password to check.
+
+    Returns:
+        True if the credentials are valid, False otherwise.
+
+    Raises:
+        ClientConnectionError: If a controller could not be reached.
     """
     try:
-        await client.command("VERSION")
+        async with CommandClient(host, username, password, ssl=ssl) as client:
+            await client.command("VERSION")
+            return True
+    except LoginFailedError:
         return False
-    except LoginRequiredError:
-        return True
