d0e86195f841409783212e4adbde84808bc71f5d
James Smith <james@loopj.com>
2023-07-31 14:12:03 -0700

    Add methods for IIntrospection.GetTypes and IIntrospection.GetInterfaces

--- src/aiovantage/config_client/interfaces/introspection/__init__.py
+++ src/aiovantage/config_client/interfaces/introspection/__init__.py
@@ -1,7 +1,11 @@
 """Introspection interface (IIntrospection) method definitions."""
 
+from .get_interfaces import GetInterfaces
+from .get_types import GetTypes
 from .get_version import GetVersion
 
 __all__ = [
+    "GetInterfaces",
+    "GetTypes",
     "GetVersion",
 ]
---
+++ src/aiovantage/config_client/interfaces/introspection/get_interfaces.py
@@ -0,0 +1,28 @@
+"""IIntrospection.GetInterfaces method definition."""
+
+from dataclasses import dataclass, field
+from typing import ClassVar, List, Optional
+
+
+@dataclass
+class Interface:
+    """Object interface definition."""
+
+    name: str = field(metadata={"name": "Name"})
+    version: str = field(metadata={"name": "Version"})
+    id: int = field(metadata={"name": "IID"})
+
+
+@dataclass
+class GetInterfaces:
+    """IIntrospection.GetInterfaces method definition."""
+
+    interface: ClassVar[str] = "IIntrospection"
+    call = None
+    return_value: Optional[List[Interface]] = field(
+        default=None,
+        metadata={
+            "name": "Interface",
+            "wrapper": "return",
+        },
+    )
