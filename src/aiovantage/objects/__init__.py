0d0f012f0d5c282876f0c7bc33cd68f057b74e39
James Smith <james@loopj.com>
2024-12-13 10:28:24 -0800

    Remove __all__ from objects epxort

--- src/aiovantage/objects/__init__.py
+++ src/aiovantage/objects/__init__.py
@@ -1,5 +1,9 @@
 """Vantage object types."""
 
+# fmt: off
+# ruff: noqa: I001, F401
+
+# Make all object classes available in aioVantage.objects
 from .system_object import SystemObject
 from .system_object.button import Button
 from .system_object.gmem import GMem
@@ -9,31 +13,15 @@ from .system_object.location_object.blind import Blind
 from .system_object.location_object.blind_group import BlindGroup
 from .system_object.location_object.custom_device import CustomDevice
 from .system_object.location_object.custom_device.child_device import ChildDevice
-from .system_object.location_object.custom_device.child_device.somfy_rs_485_group_child import (
-    SomfyRS485GroupChild,
-)
-from .system_object.location_object.custom_device.child_device.somfy_rs_485_shade_child import (
-    SomfyRS485ShadeChild,
-)
-from .system_object.location_object.custom_device.child_device.somfy_urtsi_2_group_child import (
-    SomfyURTSI2GroupChild,
-)
-from .system_object.location_object.custom_device.child_device.somfy_urtsi_2_shade_child import (
-    SomfyURTSI2ShadeChild,
-)
+from .system_object.location_object.custom_device.child_device.somfy_rs_485_group_child import SomfyRS485GroupChild
+from .system_object.location_object.custom_device.child_device.somfy_rs_485_shade_child import SomfyRS485ShadeChild
+from .system_object.location_object.custom_device.child_device.somfy_urtsi_2_group_child import SomfyURTSI2GroupChild
+from .system_object.location_object.custom_device.child_device.somfy_urtsi_2_shade_child import SomfyURTSI2ShadeChild
 from .system_object.location_object.custom_device.parent_device import ParentDevice
-from .system_object.location_object.custom_device.parent_device.port_device import (
-    PortDevice,
-)
-from .system_object.location_object.custom_device.parent_device.port_device.somfy_rs_485_sdn_20_port import (
-    SomfyRS485SDN20Port,
-)
-from .system_object.location_object.custom_device.parent_device.port_device.somfy_urtsi_2_port import (
-    SomfyURTSI2Port,
-)
-from .system_object.location_object.custom_device.parent_device.port_device.vantage_dmx_gateway import (
-    VantageDmxGateway,
-)
+from .system_object.location_object.custom_device.parent_device.port_device import PortDevice
+from .system_object.location_object.custom_device.parent_device.port_device.somfy_rs_485_sdn_20_port import SomfyRS485SDN20Port
+from .system_object.location_object.custom_device.parent_device.port_device.somfy_urtsi_2_port import SomfyURTSI2Port
+from .system_object.location_object.custom_device.parent_device.port_device.vantage_dmx_gateway import VantageDmxGateway
 from .system_object.location_object.dry_contact import DryContact
 from .system_object.location_object.load import Load
 from .system_object.location_object.load_group import LoadGroup
@@ -46,39 +34,23 @@ from .system_object.location_object.sensor.temperature import Temperature
 from .system_object.location_object.station_object import StationObject
 from .system_object.location_object.station_object.contact_input import ContactInput
 from .system_object.location_object.station_object.din_station import DINStation
-from .system_object.location_object.station_object.din_station.din_contact_input import (
-    DINContactInput,
-)
-from .system_object.location_object.station_object.din_station.din_high_voltage_relay_station import (
-    DINHighVoltageRelayStation,
-)
-from .system_object.location_object.station_object.din_station.din_low_voltage_relay_station import (
-    DINLowVoltageRelayStation,
-)
+from .system_object.location_object.station_object.din_station.din_contact_input import DINContactInput
+from .system_object.location_object.station_object.din_station.din_high_voltage_relay_station import DINHighVoltageRelayStation
+from .system_object.location_object.station_object.din_station.din_low_voltage_relay_station import DINLowVoltageRelayStation
 from .system_object.location_object.station_object.eq_ctrl import EqCtrl
 from .system_object.location_object.station_object.eq_ux import EqUX
-from .system_object.location_object.station_object.high_voltage_relay_station import (
-    HighVoltageRelayStation,
-)
+from .system_object.location_object.station_object.high_voltage_relay_station import HighVoltageRelayStation
 from .system_object.location_object.station_object.keypad import Keypad
 from .system_object.location_object.station_object.keypad.dimmer import Dimmer
-from .system_object.location_object.station_object.keypad.dual_relay_station import (
-    DualRelayStation,
-)
-from .system_object.location_object.station_object.keypad.scene_point_relay import (
-    ScenePointRelay,
-)
-from .system_object.location_object.station_object.low_voltage_relay_station import (
-    LowVoltageRelayStation,
-)
+from .system_object.location_object.station_object.keypad.dual_relay_station import DualRelayStation
+from .system_object.location_object.station_object.keypad.scene_point_relay import ScenePointRelay
+from .system_object.location_object.station_object.low_voltage_relay_station import LowVoltageRelayStation
 from .system_object.location_object.station_object.qis_blind import QISBlind
 from .system_object.location_object.station_object.qube_blind import QubeBlind
 from .system_object.location_object.station_object.rs232_station import RS232Station
 from .system_object.location_object.station_object.rs485_station import RS485Station
 from .system_object.location_object.station_object.thermostat import Thermostat
-from .system_object.location_object.station_object.vantage_dmx_dali_gateway import (
-    VantageDmxDaliGateway,
-)
+from .system_object.location_object.station_object.vantage_dmx_dali_gateway import VantageDmxDaliGateway
 from .system_object.location_object.vantage_ddg_color_load import VantageDDGColorLoad
 from .system_object.location_object.vantage_dg_color_load import VantageDGColorLoad
 from .system_object.master import Master
@@ -86,70 +58,7 @@ from .system_object.module import Module
 from .system_object.module_gen2 import ModuleGen2
 from .system_object.power_profile import PowerProfile
 from .system_object.power_profile.dc_power_profile import DCPowerProfile
-from .system_object.power_profile.dc_power_profile.pwm_power_profile import (
-    PWMPowerProfile,
-)
+from .system_object.power_profile.dc_power_profile.pwm_power_profile import PWMPowerProfile
 from .system_object.station_bus import StationBus
 from .system_object.task import Task
 from .types import Parent
-
-__all__ = [
-    "AnemoSensor",
-    "Area",
-    "Blind",
-    "BlindGroup",
-    "Button",
-    "ChildDevice",
-    "ContactInput",
-    "CustomDevice",
-    "DCPowerProfile",
-    "Dimmer",
-    "DINContactInput",
-    "DINHighVoltageRelayStation",
-    "DINLowVoltageRelayStation",
-    "DINStation",
-    "DryContact",
-    "DualRelayStation",
-    "EqCtrl",
-    "EqUX",
-    "GMem",
-    "HighVoltageRelayStation",
-    "Keypad",
-    "LightSensor",
-    "Load",
-    "LoadGroup",
-    "LocationObject",
-    "LowVoltageRelayStation",
-    "Master",
-    "Module",
-    "ModuleGen2",
-    "OmniSensor",
-    "Parent",
-    "ParentDevice",
-    "PortDevice",
-    "PowerProfile",
-    "PWMPowerProfile",
-    "QISBlind",
-    "QubeBlind",
-    "RelayBlind",
-    "RS232Station",
-    "RS485Station",
-    "ScenePointRelay",
-    "Sensor",
-    "SomfyRS485GroupChild",
-    "SomfyRS485SDN20Port",
-    "SomfyRS485ShadeChild",
-    "SomfyURTSI2GroupChild",
-    "SomfyURTSI2Port",
-    "SomfyURTSI2ShadeChild",
-    "StationBus",
-    "StationObject",
-    "SystemObject",
-    "Task",
-    "Temperature",
-    "Thermostat",
-    "VantageDDGColorLoad",
-    "VantageDGColorLoad",
-    "VantageDmxDaliGateway",
-    "VantageDmxGateway",
-]
