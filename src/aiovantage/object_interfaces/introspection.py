3ec699584fa3f1f72fdf906b474ac90069506974
James Smith <james@loopj.com>
2025-01-10 14:40:49 -0800

    Add methds to introspectiona

--- src/aiovantage/object_interfaces/introspection.py
+++ src/aiovantage/object_interfaces/introspection.py
@@ -16,16 +16,42 @@ class IntrospectionInterface(Interface):
         RootFs = 1
         Application = 2
 
-    class FirmwareVersion(NamedTuple):
+    class LicenseType(IntEnum):
+        """License types."""
+
+        Unknown = 0
+        Equinox = 1
+        All = -1
+
+    class FirmwareVersionResponse(NamedTuple):
         """A firmware version response."""
 
         rcode: int
-        image: int
+        image: "IntrospectionInterface.Firmware"
         version: str
         size: int
 
+    class LicenseInfoResponse(NamedTuple):
+        """A license info response."""
+
+        rcode: int
+        type: "IntrospectionInterface.LicenseType"
+        used: int
+        total: int
+
+    @method("Introspection.GetAppControllers")
+    async def get_app_controllers(self) -> str:
+        """Get a list of controllers in application mode, exclidng this controller.
+
+        Returns:
+            A comma-separated list of controller numbers.
+        """
+        # INVOKE <id> Introspection.GetAppControllers
+        # -> R:INVOKE <id> <rcode> Introspection.GetAppControllers <controllers>
+        return await self.invoke("Introspection.GetAppControllers")
+
     @method("Introspection.GetFirmwareVersion")
-    async def get_firmware_version(self, image: Firmware) -> FirmwareVersion:
+    async def get_firmware_version(self, image: Firmware) -> FirmwareVersionResponse:
         """Get the firmware version.
 
         Args:
@@ -34,6 +60,17 @@ class IntrospectionInterface(Interface):
         # INVOKE <id> Introspection.GetFirmwareVersion <image>
         # -> R:INVOKE <id> <rcode> Introspection.GetFirmwareVersion <image> <version>
         return await self.invoke("Introspection.GetFirmwareVersion", image)
+
+    @method("Introspection.GetLicenseInfo")
+    async def get_license_info(self, type: LicenseType) -> LicenseInfoResponse:
+        """Get license information.
+
+        Args:
+            type: The license type to get information for.
+        """
+        # INVOKE <id> Introspection.GetLicenseInfo <type>
+        # -> R:INVOKE <id> <rcode> Introspection.GetLicenseInfo <type> <used> <total>
+        return await self.invoke("Introspection.GetLicenseInfo", type)
 
     async def get_application_version(self) -> str:
         """Get the application firmware version."""
