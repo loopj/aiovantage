abdd7439abf92d243ee68d3baf2a1323a38d0cdf
James Smith <james@loopj.com>
2025-01-13 09:46:38 -0800

    Drop group methods on controllers

--- src/aiovantage/controllers/blind_groups.py
+++ src/aiovantage/controllers/blind_groups.py
@@ -1,15 +1,8 @@
 """Controller holding and managing Vantage blind groups."""
 
-from aiovantage.objects import (
-    BlindGroup,
-    ChildDevice,
-    SomfyRS485GroupChild,
-    SomfyURTSI2GroupChild,
-)
-from aiovantage.query import QuerySet
+from aiovantage.objects import BlindGroup, SomfyRS485GroupChild, SomfyURTSI2GroupChild
 
 from .base import BaseController
-from .blinds import BlindTypes
 
 # The various "blind group" object types don't all inherit from the same base class,
 # so for typing purposes we'll use a union of all the types.
@@ -20,22 +13,3 @@ class BlindGroupsController(BaseController[BlindGroupTypes]):
     """Controller holding and managing Vantage blind groups."""
 
     vantage_types = (BlindGroup, SomfyRS485GroupChild, SomfyURTSI2GroupChild)
-
-    def blinds(self, vid: int) -> QuerySet[BlindTypes]:
-        """Return a queryset of all blinds in this blind group."""
-        blind_group = self[vid]
-        if isinstance(blind_group, BlindGroup):
-
-            def _filter1(blind: BlindTypes) -> bool:
-                return blind.id in blind_group.blind_table
-
-            return self._vantage.blinds.filter(_filter1)
-
-        # Otherwise, use the parent id to filter
-        def _filter2(blind: BlindTypes) -> bool:
-            if isinstance(blind, ChildDevice):
-                return blind.parent.id == blind_group.id
-
-            return False
-
-        return self._vantage.blinds.filter(_filter2)
