0c03c2ae11b97607decca1ee6a971176333f5f98
James Smith <james@loopj.com>
2025-01-14 11:43:01 -0800

    Prepare for more thermostat types

--- src/aiovantage/controllers/thermostats.py
+++ src/aiovantage/controllers/thermostats.py
@@ -4,8 +4,12 @@ from aiovantage.objects import Thermostat
 
 from .base import BaseController
 
+# The various "thermostat" object types don't all inherit from the same base class,
+# so for typing purposes we'll use a union of all the types.
+ThermostatTypes = Thermostat
 
-class ThermostatsController(BaseController[Thermostat]):
+
+class ThermostatsController(BaseController[ThermostatTypes]):
     """Controller holding and managing thermostats."""
 
     vantage_types = (Thermostat,)
