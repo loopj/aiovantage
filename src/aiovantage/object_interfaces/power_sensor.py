043d74fc2f4b5da657832867386a3ca46de00d45
James Smith <james@loopj.com>
2025-01-18 12:41:49 -0800

    Add CurrentSensor, PowerSensor interfaces

---
+++ src/aiovantage/object_interfaces/power_sensor.py
@@ -0,0 +1,43 @@
+"""Interface for querying and controlling power sensors."""
+
+from decimal import Decimal
+
+from .base import Interface, method
+
+
+class PowerSensorInterface(Interface):
+    """Interface for querying and controlling power sensors."""
+
+    interface_name = "PowerSensor"
+
+    # Methods
+    @method("PowerSensor.GetPower")
+    @method("PowerSensor.GetPower")
+    async def get_power(self, *, hw: bool = False) -> Decimal:
+        """Get the value of a power sensor.
+
+        Args:
+            hw: Fetch the value from hardware instead of cache.
+
+        Returns:
+            The value of the power sensor, in Watts.
+        """
+        # INVOKE <id> PowerSensor.GetPower
+        # -> R:INVOKE <id> <level> PowerSensor.GetPower
+        return await self.invoke(
+            "PowerSensor.GetPowerHW" if hw else "PowerSensor.GetPower"
+        )
+
+    @method("PowerSensor.SetPower")
+    async def set_power(self, value: Decimal, *, sw: bool = False) -> None:
+        """Set the value of a power sensor.
+
+        Args:
+            value: The value to set, in Watts.
+            sw: Set the cached value instead of the hardware value.
+        """
+        # INVOKE <id> PowerSensor.SetPower <level>
+        # -> R:INVOKE <id> <rcode> PowerSensor.SetPower <level>
+        await self.invoke(
+            "PowerSensor.SetPowerSW" if sw else "PowerSensor.SetPower", value
+        )
