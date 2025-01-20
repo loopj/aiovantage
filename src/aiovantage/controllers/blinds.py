9b25285c84370a840966b0624c3fa73d13057090
James Smith <james@loopj.com>
2025-01-20 13:33:04 -0800

    Add somfy blind groups as blinds

--- src/aiovantage/controllers/blinds.py
+++ src/aiovantage/controllers/blinds.py
@@ -1,22 +1,28 @@
 """Controller holding and managing Vantage blinds."""
 
 from aiovantage.objects import (
+    Blind,
     BlindGroup,
     QISBlind,
     QubeBlind,
     RelayBlind,
+    SomfyRS485GroupChild,
     SomfyRS485ShadeChild,
+    SomfyURTSI2GroupChild,
     SomfyURTSI2ShadeChild,
 )
 
 from .base import BaseController
 
 BlindTypes = (
-    BlindGroup
+    Blind
+    | BlindGroup
     | QISBlind
     | QubeBlind
     | RelayBlind
+    | SomfyRS485GroupChild
     | SomfyRS485ShadeChild
+    | SomfyURTSI2GroupChild
     | SomfyURTSI2ShadeChild
 )
 
@@ -25,10 +31,13 @@ class BlindsController(BaseController[BlindTypes]):
     """Controller holding and managing Vantage blinds."""
 
     vantage_types = (
+        Blind,
         BlindGroup,
         QISBlind,
         QubeBlind,
         RelayBlind,
+        SomfyRS485GroupChild,
         SomfyRS485ShadeChild,
+        SomfyURTSI2GroupChild,
         SomfyURTSI2ShadeChild,
     )
